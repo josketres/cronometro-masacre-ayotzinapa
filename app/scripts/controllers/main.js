efbf42bccd10c0d38ee8c2e94661342e460930cb
Josue Zarzosa <josketres@gmail.com>
2014-10-27 21:19:34 +0100

    Initial commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name cronometroMasacreAyotzinapaApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the cronometroMasacreAyotzinapaApp
+ */
+angular.module('cronometroMasacreAyotzinapaApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
